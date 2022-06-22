2da1af7297492eeb15a55ea710e6785605e32ec0
Gospel Udechukwu <gosmajesty@gmail.com>
2022-06-22 02:57:35 +0100

    Application was built with angular material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserdetailComponent } from './Components/userdetail/userdetail.component';
+import { UsersComponent } from './Components/users/users.component';
+import { UserResolver } from './Services/user.resolver';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', component: UsersComponent },
+  { path: 'user/:uuid', component: UserdetailComponent, resolve: {resolvedResponse: UserResolver} },
+  { path: '**', redirectTo: 'users' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './Components/users/users.component';
+import { UserdetailComponent } from './Components/userdetail/userdetail.component';
+import { HttpClientModule } from '@angular/common/http'
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserdetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    LeafletModule
   ],
   providers: [],
   bootstrap: [AppComponent]
